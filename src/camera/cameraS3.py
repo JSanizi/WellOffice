ea6a27a167ecf52ac874b092c43b3f052cd21932
gusls02 <A01721747@tec.mx>
2024-12-04 13:34:26 +0100

    update image storage expiration

--- src/camera/cameraS3.py
+++ src/camera/cameraS3.py
@@ -1,10 +1,20 @@
 import time
 import subprocess
-from datetime import datetime
+from datetime import datetime, timedelta
 import boto3 # type: ignore
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient  # type: ignore
 import json
+import os
 
+def delete_expired_local_image(folder, retention_time):
+    current_time = datetime.now()
+    for file_name in os.listdir(folder):
+        file_path = os.path.join(folder, file_name)
+        if os.path.isfile(file_path):
+            creation_time = datetime.fromtimestamp(os.path.getctime(file_path))
+            if current_time - creation_time > retention_time:
+                os.remove(file_path)
+                print(f"Deleted expired file: {file_name}")
 
 
 def capture_image_and_upload():
@@ -29,26 +39,34 @@ def capture_image_and_upload():
     print("Connected to AWS IoT Core")
 
     local_storage = "/home/group4/images"
+    retention_time = timedelta(minutes=30)
 
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    file_path = f"{local_storage}/image_{timestamp}.jpg"
+    while True:
+        try:
 
-    subprocess.run(["libcamera-still", "-o", file_path])
-    print(f"Image saved to {file_path}")
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            file_path = f"{local_storage}/image_{timestamp}.jpg"
 
-    s3_key = f"images/photo_{timestamp}.jpg"
-    s3_client.upload_file(file_path, s3_bucket, s3_key)
-    print(f"Uploaded {file_path} to S3 as {s3_key}")
+            subprocess.run(["libcamera-still", "-o", file_path])
+            print(f"Image saved to {file_path}")
 
-    message = {"bucket": s3_bucket,
-               "key": s3_key,
-               "timestamp": timestamp}
-    
-    myMQTTClient.publish(topic, json.dumps(message), 1)
-    print("Published notification to AWS IoT Core")
+            s3_key = f"images/photo_{timestamp}.jpg"
+            s3_client.upload_file(file_path, s3_bucket, s3_key)
+            print(f"Uploaded {file_path} to S3 as {s3_key}")
 
-    return file_path
+            message = {"bucket": s3_bucket,
+                    "key": s3_key,
+                    "timestamp": timestamp}
+            
+            myMQTTClient.publish(topic, json.dumps(message), 1)
+            print("Published notification to AWS IoT Core")
 
+            delete_expired_local_image(local_storage, retention_time)
+
+            return file_path
+
+        except Exception as e:
+            print(f"Error:{e}")
 """ try:
     while True:
         capture_image_and_upload()

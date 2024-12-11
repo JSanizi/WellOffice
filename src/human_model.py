6ff52382b9010eff97b7c612059f3e31acef019d
gusls02 <143132245+gusls02@users.noreply.github.com>
2024-12-11 12:08:44 +0100

    testing HAR Fix

--- src/human_model.py
+++ src/human_model.py
@@ -94,7 +94,7 @@ def human_inference(aux_image):
 
     # Check if YOLO found any detections
     if yolo_detections:
-        print("YOLO detected objects." + yolo_detections)
+        print("YOLO detected objects." + yolo_detections[0])
         annotate_and_save(
             input_image_path,
             yolo_detections,
@@ -122,7 +122,7 @@ def human_inference(aux_image):
             second_model_detections.append((x_min, y_min, x_max, y_max))
 
         if second_model_detections:
-            print("Second model detection: " + second_model_detections)
+            print("Second model detection: " + second_model_detections[0])
             annotate_and_save(
                 input_image_path,
                 second_model_detections,
